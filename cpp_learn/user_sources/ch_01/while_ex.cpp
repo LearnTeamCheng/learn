c4bf513ec028b37cf5681d990da467caff81d66d
chengsn <chengsn@ihep.ac.cn>
2025-03-04 18:25:36 +0800

    init:init

---
+++ cpp_learn/user_sources/ch_01/while_ex.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    int sum = 0, value = 0, i = 0;
+    while (std::cin >> value) {
+        sum += value;
+        i++;
+    }
+    std::cout << "Sum of " << i0 << " numbers is " << sum << std ::endl;
+    return 0;
+}
